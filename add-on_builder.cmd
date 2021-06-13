b6c6fab97fcd9c6d101a9395dda213c5152185a3
Username-N00b-is-not-available <59409713+Username-N00b-is-not-available@users.noreply.github.com>
2021-06-13 20:10:49 +0300

    Automatization
    Restructured the folders and added a script by @mgrinzPlayer which builds the pk3 and boa files using 7za.

---
+++ add-on_builder.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+del /f /q *.pk3 *.boa 2>nul
+del /f /s release 2>nul
+
+pushd source
+for /F %%I in ('dir /ad /b') do (
+  pushd "%%I"
+  "..\..\7za.exe" a -tzip -mx1 "..\..\release\addons\%%I.pk3" -x!addoninfo.txt -x!gameinfo.txt -x!preview *
+  "..\..\7za.exe" a -tzip -mx1 "..\..\release\addon_%%I.boa" addoninfo.txt gameinfo.txt preview
+  popd
+)
+popd
